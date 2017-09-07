ae30f402254db827b6b30208c1de1a79bac52e36
Nero <13701214831@163.com>
2017-09-07 19:54:44 +0800

    myWorkList

---
+++ js/navi.js
@@ -0,0 +1,55 @@
+/**
+ * Created by Nero on 2018/9/06.
+ */
+    var vm = new Vue({
+        el: "#mainContainer",
+        data: {
+            dataList: []
+        },
+        mounted: function () {
+            this.$nextTick(function () {
+                vm.getListData();
+                window.addEventListener('scroll', vm.scrollTop);
+                window.onload = function() {
+                    $('.loader-container').hide();
+                }
+            });
+        },
+        methods: {
+            //获取数据
+            getListData: function () {
+                var _this = this;
+                this.$http.get('assets/data/navi.json')
+                    .then(function (res) {
+                        _this.dataList = res.data.result.navig;
+                    })
+                    .catch(function (res) {
+                    })
+            },
+            //返回首页
+            backHome: function () {
+                window.location.href = "http://www.zzyfun.com";
+            },
+
+            //侧边导航切换内容
+            switchContent: function ($index) {
+                $('.left-container li:eq('+$index+')').addClass('nav-actived').siblings().removeClass('nav-actived');
+                $('.content-item:eq(' + $index + ')').removeClass('hide').siblings().addClass('hide');
+            },
+
+            //滚动条距离顶部的距离
+            scrollTop: function() {
+                if($(window).scrollTop() > 100) {
+                    $('.back-top').fadeIn(300);
+                }else {
+                    $('.back-top').fadeOut(300);
+                }
+            },
+
+            //返回顶部
+            goTop: function() {
+                $("html,body").animate({scrollTop:0}, 500);
+                return false;
+            }
+        }
+    });
